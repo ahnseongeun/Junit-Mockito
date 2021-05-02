828824393b74763bb2137dedbd1cf8586cfe0157
ahnseongeun <ast3138@naver.com>
2021-05-03 02:13:09 +0900

    2021-5-3 ValueSource와 ConvertWith 활용하기

--- src/test/java/com/test_java/lecture6_반복적인테스트/studyTest6.java
+++ src/test/java/com/test_java/lecture6_반복적인테스트/studyTest6.java
@@ -2,7 +2,12 @@ package com.test_java.lecture6_반복적인테스트;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class studyTest6 {
 
@@ -25,7 +30,51 @@ public class studyTest6 {
     @DisplayName("스터디 만들기")
     @ParameterizedTest(name = "{index} {displayName} message={0}")
     @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+    @EmptySource //빈 값 넣어 주기
+    @NullSource //Null 값 넣어 주기
+    //@NullAndEmptySource 빈 값과 Null 값 동시에 넣어주기
     void parameterizedTest(String message){
+        System.out.println(message);
+    }
+
+    /**
+     * value int 사용
+     * @param message
+     */
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 30})
+    void parameterizedTest2(Integer message){
+        System.out.println(message);
+    }
+
+    /**
+     * converter를 사용해서 ValueSource로 받은 int를 객체로 변환이 가능하다.
+     * @param study
+     */
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 30})
+    void parameterizedTest2(@ConvertWith(StudyConverter.class) Study study){
+        System.out.println(study.getLimit());
+    }
+
+    static class StudyConverter extends SimpleArgumentConverter{
+
+        @Override
+        protected Object convert(Object source, Class<?> targetType) throws ArgumentConversionException {
+            assertEquals(Study.class, targetType,"Can only convert to Study");
+            return new Study(Integer.parseInt(source.toString()));
+        }
+    }
+
+    /**
+     * CsvSource를 사용하면 다양한 타입의 인자값들을 넘겨줄 수 있다.
+     */
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(ints = {10, 20, 30})
+    void parameterizedTest3(Integer message){
         System.out.println(message);
     }
 
