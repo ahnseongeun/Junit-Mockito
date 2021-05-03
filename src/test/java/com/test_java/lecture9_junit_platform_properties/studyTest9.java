af05789d11246297589cc66c5445e260a5a5d735
ahnseongeun <ast3138@naver.com>
2021-05-04 01:55:14 +0900

    2021-5-3 테스트순서

---
+++ src/test/java/com/test_java/lecture9_junit_platform_properties/studyTest9.java
@@ -0,0 +1,54 @@
+package com.test_java.lecture9_junit_platform_properties;
+
+import com.test_java.lecture5_커스텀태킹.FastTest;
+import com.test_java.lecture5_커스텀태킹.SlowTest;
+import com.test_java.lecture7_테스트인스턴스.Study;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * test의 resources를 설정한다.
+ * File -> Project Structure -> module -> test resource
+ * resources에서는
+ * 테스트 인스턴스 라이프사이클 설정
+ * 확장팩 자동 감지 기능
+ * @Disabled 무시하고 실행하기
+ * 테스트 이름 표기 전략 실행
+ */
+public class studyTest9 {
+
+    int value = 0;
+
+    @FastTest
+    @DisplayName("스터디 만들기 fast")
+    void create_new_study(){
+        com.test_java.lecture7_테스트인스턴스.Study actual = new Study(++value);
+        assertThat(actual.getLimit()).isGreaterThan(0);
+        System.out.println("스터디 만들기 fast" + value);
+        //객체의 해시코드보기
+        System.out.println(this);
+    }
+
+    @SlowTest
+    @DisplayName("스터디 만들기 slow")
+        //fast에서 value값을 증가 시켜줬지만 증가되지 않은 0이 출력된다.
+    void create_new_study_again(){
+        System.out.println("스터디 만들기 slow" + value);
+        System.out.println(this);
+    }
+
+
+    @BeforeAll
+    static void beforeTestAll(){
+        System.out.println("before all test");
+    }
+
+    @AfterAll
+    static void afterTestAll(){
+        System.out.println("after all test");
+    }
+}
